6ce34f681af3b12fd711d4b8d0ab618d0368fd1c
steph <steph@SurfacePro.fritz.box>
2018-06-02 15:16:27 +0200

    Aufgabe 2 - Habe sie in der AnimalBoarding.java geschrieben, da ich von außerhalb nicht auf die Liste zugreifen konnte.

--- src/org/mytuc/dstruc/group12/AnimalBoarding.java
+++ src/org/mytuc/dstruc/group12/AnimalBoarding.java
@@ -8,6 +8,8 @@ package org.mytuc.dstruc.group12;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AnimalBoarding { 
@@ -100,6 +102,7 @@ public class AnimalBoarding {
         } 
         return le; 
     } 
+    
 
     public void writeList() { 
         ListElement le = startElem; 
@@ -112,6 +115,7 @@ public class AnimalBoarding {
     
     public static void main(String[] args) throws IOException  { 
         AnimalBoarding list = new AnimalBoarding();
+        
         Scanner diskScanner = new Scanner(new File ("tierliste.txt"));
     	 while (diskScanner.hasNext()) {
     		list.addLast (diskScanner.nextLine());
@@ -119,15 +123,174 @@ public class AnimalBoarding {
     	}
     	 diskScanner.close(); 
         
-    	// list.addLast ("Pferd;Axel;m;150;100"); // Test zum manuellen einfügen in Liste
+        
+      
+    	//list.addLast("Pferd;Axel;m;150;100"); // Test zum manuellen einfügen in Liste
     
        
     	
-        // System.out.println("erstes Element: " + list.getFirstElem().getObj()); 
+        //System.out.println("erstes Element: " + list.getFirstElem().getObj()); 
        // System.out.println("letztes Element: " + list.getLastElem().getObj()); 
        // System.out.println("vorletztes Element: " + list.getLastElem().getPrevElem().getObj()); 
         list.writeList(); 
+        
+        //Aufgabe 2
+        
+        ListElement first = list.getFirstElem();
+        ListElement last = list.getLastElem();
+        
+        int PferdQuantM = 0;
+        int PferdQuantW = 0;
+        int MausQuantM = 0;
+        int MausQuantW = 0;
+        int TigerQuantM = 0;
+        int TigerQuantW = 0;
+        int SpinneQuantM = 0;
+        int SpinneQuantW = 0;
+        int HaseQuantM = 0;
+        int HaseQuantW = 0;
+        int KuhQuantM = 0;
+        int KuhQuantW = 0;
+        int ZebraQuantM = 0;
+        int ZebraQuantW = 0;
+        int NilpferdQuantM = 0;
+        int NilpferdQuantW = 0;
+        int KrokodilQuantM = 0;
+        int KrokodilQuantW = 0;
+        
+        List<Object> Beiboot = new LinkedList<Object>();
+        
+        while (first.getObj()== null) first = first.getNextElem();
+        do {
+        String firstStr = (String) first.getObj();
+        String StrArr[] = firstStr.split(";");
+        
+        if (StrArr[0].contains("Pferd") || StrArr[0].contains("Einhorn"))
+        {
+        	if (StrArr[2].contains("m") && PferdQuantM<1) PferdQuantM++;
+        	        	
+        	else if (StrArr[2].contains("w") && PferdQuantW<1) PferdQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        	
+        else if (StrArr[0].contains("Maus"))
+        {
+        	if (StrArr[2].contains("m") && MausQuantM<1) MausQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && MausQuantW<1) MausQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        else if (StrArr[0].contains("Tiger"))
+        {
+        	if (StrArr[2].contains("m") && TigerQuantM<1) TigerQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && TigerQuantW<1) TigerQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        else if (StrArr[0].contains("Spinne"))
+        {
+        	if (StrArr[2].contains("m") && SpinneQuantM<1) SpinneQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && SpinneQuantW<1) SpinneQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        else if (StrArr[0].contains("Hase"))
+        {
+        	if (StrArr[2].contains("m") && HaseQuantM<1) HaseQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && HaseQuantW<1) HaseQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        else if (StrArr[0].contains("Kuh"))
+        {
+        	if (StrArr[2].contains("m") && KuhQuantM<1) KuhQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && KuhQuantW<1) KuhQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        else if (StrArr[0].contains("Zebra"))
+        {
+        	if (StrArr[2].contains("m") && ZebraQuantM<1) ZebraQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && ZebraQuantW<1) ZebraQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        else if (StrArr[0].contains("Nilpferd"))
+        {
+        	if (StrArr[2].contains("m") && NilpferdQuantM<1) NilpferdQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && NilpferdQuantW<1) NilpferdQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        else if (StrArr[0].contains("Krokodil"))
+        {
+        	if (StrArr[2].contains("m") && KrokodilQuantM<1) KrokodilQuantM++;
+        	
+        	else if (StrArr[2].contains("w") && KrokodilQuantW<1) KrokodilQuantW++;
+        	
+        	else
+        	{
+        		Beiboot.add(first);
+        		list.delete(first);
+        	}
+        }
+        
+        first = first.getNextElem();
+        
+        }while (first != last.getNextElem());
+        
     } 
+    
+    
+    
+    
 } 
 
 class ListElement { 
